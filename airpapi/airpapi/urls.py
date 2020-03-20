e9bccb0356c11fc30773f4934a556bc269ed2a3c
a8081 <amartinezr98@protonmail.com>
2020-03-20 11:24:21 +0100

    Modules and settings

--- airpapi/airpapi/urls.py
+++ airpapi/airpapi/urls.py
@@ -14,8 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from rest_framework import routers
+from processor import views
+from rest_framework_swagger.views import get_swagger_view
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+# Swagger
+schema_view = get_swagger_view(title='AIRP@PI documentation')
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+# 
+# urlpatterns = [
+#     re_path(r'^i18n/', include('django.conf.urls.i18n')),
+#     re_path(r'^cas/', include('mama_cas.urls')),
+#     re_path(r'^admin/', admin.site.urls),
+# ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema_view),
+    re_path(r'^auth/', include('djoser.urls')),
+    # re_path(r'^api-auth/', include('rest_framework.urls',namespace='rest_framework')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    re_path(r'^auth/', include('djoser.urls.jwt')),
+    path('v1/api/', include(router.urls)),
 ]

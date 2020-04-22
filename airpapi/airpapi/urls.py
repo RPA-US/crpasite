585836d5f99ad8d18589b98a5f05c8db8a42c40d
a8081 <amartinezr98@protonmail.com>
2020-04-22 20:50:44 +0200

    Init first deploy test

--- airpapi/airpapi/urls.py
+++ airpapi/airpapi/urls.py
@@ -17,28 +17,50 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework import routers
 from processor import views
-from rest_framework_swagger.views import get_swagger_view
+from consumer import views as consumer_views
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+# from rest_framework_swagger.views import get_swagger_view
 # from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Swagger
-schema_view = get_swagger_view(title='AIRP@PI documentation')
+# Swagger: schema_view = get_swagger_view(title='AIRP@PI documentation')
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-# 
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
+
 # urlpatterns = [
 #     re_path(r'^i18n/', include('django.conf.urls.i18n')),
 #     re_path(r'^cas/', include('mama_cas.urls')),
 #     re_path(r'^admin/', admin.site.urls),
 # ]
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="AIRP@PI API",
+      default_version='v1',
+      description="AI-RPA API platform",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="antmarroj@us.es"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('docs/', schema_view),
     re_path(r'^auth/', include('djoser.urls')),
-    # re_path(r'^api-auth/', include('rest_framework.urls',namespace='rest_framework')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     re_path(r'^auth/', include('djoser.urls.jwt')),
-    path('v1/api/', include(router.urls)),
+    re_path(r'^scale/text', consumer_views.text_classification, name='text-classification'),
+    re_path(r'^scale/image', consumer_views.image, name='image'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
+    # path('docs/', schema_view),
+    # path('v1/api', include(router.urls)),
+    # re_path(r'^api-auth/', include('rest_framework.urls',namespace='rest_framework')),
 ]
--- airpapi/consumer/views.py
+++ airpapi/consumer/views.py
@@ -1,3 +1,78 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+import scaleapi
+from drf_yasg import openapi
+from drf_yasg.app_settings import swagger_settings
+from drf_yasg.inspectors import CoreAPICompatInspector, FieldInspector, NotHandled, SwaggerAutoSchema
+from drf_yasg.utils import no_body, swagger_auto_schema
+from consumer import serializers
 
 # Create your views here.
+
+client = scaleapi.ScaleClient('test_449f3f6419964ef5ac641ca7f1d4738f')
+
+# SCALE AI
+# Test API Key: test_449f3f6419964ef5ac641ca7f1d4738f
+# Test Callback Auth Key: test_auth_e921b8e5b13845a1a64f5264022c7358
+
+
+@api_view(["GET"])
+#@permission_classes((IsAuthenticated,))
+class text_classification(viewsets.ModelViewSet):
+    def get(self, request):
+
+        # response = client.create_categorization_task(
+        #         callback_url='jozuzkie@sharklasers.com',
+        #         instruction='Categorize this movie trailer',
+        #         attachment_type='website',
+        #         attachment='https://www.youtube.com/embed/6ZfuNTqbHE8',
+        #         taxonomies={
+        #         'watch': {
+        #             'type': 'category',
+        #             'description': 'Are you going to watch the full movie?',
+        #             'choices': ['yes', 'no']
+        #         },
+        #         'genre': {
+        #             'type': 'category',
+        #             'description': 'What genre does it belong to?',
+        #             'choices': ['Action', 'Adventure', 'Comedy', 'Crime', 'Drama'],
+        #             'allow_multiple': True
+        #         }
+        #         }
+        #     )
+
+        # callback_url = self.request.callback_url
+        req_instruction = self.request.instruction
+        req_attachment_type = self.request.attachment_type
+        req_attachment = self.request.attachment
+        req_taxonomies = self.request.taxonomies
+        
+        response = client.create_categorization_task(
+                callback_url='jozuzkie@sharklasers.com',
+                instruction=req_instruction,
+                attachment_type=req_attachment_type,
+                attachment=req_attachment,
+                taxonomies=req_taxonomies
+            )
+
+        return Response(msg,status=status.HTTP_200_OK) 
+
+from rest_framework.decorators import action
+from rest_framework.parsers import FileUploadParser, MultiPartParser
+
+@swagger_auto_schema(method='get', operation_description="image GET description override")
+@swagger_auto_schema(method='post', request_body=serializers.ImageUploadSerializer)
+@swagger_auto_schema(method='delete', manual_parameters=[openapi.Parameter(
+        name='delete_form_param', in_=openapi.IN_FORM,
+        type=openapi.TYPE_INTEGER,
+        description="this should not crash (form parameter on DELETE method)"
+)])
+@action(detail=True, methods=['get', 'post', 'delete'], parser_classes=(MultiPartParser, FileUploadParser))
+def image(self, request, slug=None):
+    """
+    image method docstring
+    """
+    pass

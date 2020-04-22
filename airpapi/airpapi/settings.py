44e1fc6558786b1aab36975ba6921077118202b0
a8081 <amartinezr98@protonmail.com>
2020-04-22 21:01:16 +0200

    Database settings

--- airpapi/airpapi/settings.py
+++ airpapi/airpapi/settings.py
@@ -159,23 +159,23 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'bzpro8nmnav7lcx1wfof',
-#         'USER': 'uphbbfafkvwo5cfrwb4r',
-#         'PASSWORD': 'vo5QlKsKgQB0JeuLcDf3',
-#         'HOST': 'bzpro8nmnav7lcx1wfof-postgresql.services.clever-cloud.com',
-#         'PORT': '5432',
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bzpro8nmnav7lcx1wfof',
+        'USER': 'uphbbfafkvwo5cfrwb4r',
+        'PASSWORD': 'vo5QlKsKgQB0JeuLcDf3',
+        'HOST': 'bzpro8nmnav7lcx1wfof-postgresql.services.clever-cloud.com',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators

cc1a4ad7da7aaaedeff072d21144fcada6e4cb4e
a8081 <amartinezr98@protonmail.com>
2020-06-08 12:59:33 +0200

    initial commit

--- crpasite/crpasite/urls.py
+++ crpasite/crpasite/urls.py
@@ -1,4 +1,4 @@
-"""airpapi URL Configuration
+"""crpasite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- crpasite/crpasite/wsgi.py
+++ crpasite/crpasite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for airpapi project.
+WSGI config for crpasite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airpapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crpasite.settings')
 
 application = get_wsgi_application()

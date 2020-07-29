01f13aca3bef5e40f2c590bd0ef8ebb10bf73236
a8081 <amartinezr98@protonmail.com>
2020-07-29 14:00:33 +0200

    [feat]: Taxonomic categories views

---
+++ taxcategs/forms.py
@@ -0,0 +1,72 @@
+from django import forms
+from .models import CategoryTerm, KnowledgeSource, InputFormatSupported, Report, Comment
+
+
+class ProposalCategoryTermForm(forms.ModelForm):
+    class Meta:
+        model = CategoryTerm
+        fields = (
+            "term",
+            "description",
+            "is_tax_categ",
+            "active",
+            "tax_categ",
+            "knowledge_source",
+            "formats_supported",
+            "categoryChars",
+        )
+
+        widgets = {
+            "term": forms.TextInput(
+                attrs={
+                    "class": "single-input",
+                    "placeholder": "Term",
+                    "onfocus": "this.placeholder = ''",
+                    "onblur": "this.placeholder = 'Term'",
+                }
+            ),
+            "description": forms.Textarea(
+                attrs={
+                    "class": "single-input",
+                    "placeholder": "Description",
+                    "onfocus": "this.placeholder = ''",
+                    "onblur": "this.placeholder = 'Description'",
+                    "rows": "5",
+                }
+            ),
+            "tax_categ": forms.Select(attrs={"class": "form-control default-select"}),
+            "active": forms.CheckboxInput(
+                attrs={"class": "primary-checkbox", "checked": ""}
+            ),
+            "formats_supported": forms.CheckboxSelectMultiple(),
+             "categoryChars": forms.Textarea(
+                attrs={
+                    "class": "single-input",
+                    "placeholder": "Category characteristics (separated by commas)",
+                    "onfocus": "this.placeholder = ''",
+                    "onblur": "this.placeholder = 'Category characteristics (separated by commas)'",
+                    "rows": "5",
+                }
+            ),
+        }
+
+
+class InputFormatSupportedForm(forms.ModelForm):
+    class Meta:
+        model = InputFormatSupported
+        fields = ("name", "slug", "parent")
+
+class KnowledgeSourceForm(forms.ModelForm):
+    class Meta:
+        model = KnowledgeSource
+        fields = ("name", "url", "creator", "active")
+
+class ReportForm(forms.ModelForm):
+    class Meta:
+        model = Report
+        fields = ("decision", "result", "explanation", "reviewer")
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ("author", "category_term", "title", "text")

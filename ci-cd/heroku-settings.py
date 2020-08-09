ecc48c50e3d35bf02c396589ee02c3b048a84f4c
a8081 <amartinezr98@protonmail.com>
2020-08-09 21:34:17 +0200

    [fix]: heroku configuration settings

--- ci-cd/heroku-settings.py
+++ ci-cd/heroku-settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 INSTALLED_APPS = [
- "django.contrib.admin",
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
